afc257f07e1ab539c32806ba783661747fc0139c
kamalogudah <mcpaul2058@gmail.com>
2018-05-18 20:14:53 +0300

    Handling Requests

--- _rel/hello_cowboy_release/lib/hello_cowboy-0.1.0/ebin/hello_cowboy.app
+++ _rel/hello_cowboy_release/lib/hello_cowboy-0.1.0/ebin/hello_cowboy.app
@@ -3,7 +3,7 @@
 	{vsn, "0.1.0"},
 	{modules, ['hello_cowboy_app','hello_cowboy_sup']},
 	{registered, [hello_cowboy_sup]},
-	{applications, [kernel,stdlib]},
+	{applications, [kernel,stdlib,cowboy]},
 	{mod, {hello_cowboy_app, []}},
 	{env, []}
 ]}.
--- _rel/hello_cowboy_release/lib/hello_cowboy-0.1.0/src/hello_cowboy_app.erl
+++ _rel/hello_cowboy_release/lib/hello_cowboy-0.1.0/src/hello_cowboy_app.erl
@@ -5,7 +5,14 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	hello_cowboy_sup:start_link().
+	Dispatch = cowboy_router:compile([
+    {'_', [{"/", hello_handler, []}]}
+	]),
+	{ok, _} = cowboy:start_clear(my_http_listner,
+			 [{port, 8080}],
+			 #{env => #{dispatch => Dispatch}}
+			),
+			hello_cowboy_sup:start_link().
 
 stop(_State) ->
 	ok.
